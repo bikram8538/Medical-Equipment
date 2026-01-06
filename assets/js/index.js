0d63b7a7efd3eb44a2eb7b23140586b748c60759
bikram8538 <bikram8538@gmail.com>
2026-01-06 17:08:48 +0530

    top-quipments section done

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,3 +20,26 @@ var swiper = new Swiper(".medical-sol-swiper", {
   },
   loop: "true",
 });
+
+var swiper = new Swiper(".eqi-swiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".eqi-swiper-next",
+    prevEl: ".eqi-swiper-prev",
+  },
+
+  breakpoints: {
+    1200: { slidesPerView: 3 },
+    992: { slidesPerView: 2 },
+    768: { slidesPerView: 2 },
+    480: { slidesPerView: 1 },
+  },
+  loop: "true",
+});
